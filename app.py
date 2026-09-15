253baed9d6858c1b3819a0e2aa1e045386eb60ec
ashupuri1100-png <ashupuri1100@gmail.com>
2026-09-15 13:20:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,98 @@
+#step 1 : import important modules 
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
+import os
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score
+import streamlit as st
+
+# this streamlit is for web based application project 
+
+#web Page code
+st.title("HEALTH INSURANCE PREDICTION")
+
+img_url = "https://www.vecteezy.com/vector-art/47304210-3d-conceptual-illustration-of-health-insurance-healthcare"
+st.image(img_url)
+
+
+# LOAD DATA and ML MODEL PART
+
+#STEP 2: Load the dataset
+url = "https://raw.githubusercontent.com/ankitmisk/UIT-data/refs/heads/main/Insurance.csv"
+df = pd.read_csv(url)
+df.sample()
+
+
+#step 3 : EDA: Exploratory Data Analysis
+df.drop("Customer_ID",axis = 1,inplace = True)
+
+df['Previous_Insurance'] = df['Previous_Insurance'].map({'No':0,'Yes':1})
+df['Insurance_Bought'] = df['Insurance_Bought'].map({'No':0,'Yes':1})
+
+
+#step 4: Divide dataset into features and target
+X=df.iloc[:,:-1]
+y = df.iloc[:,-1]
+
+
+#step 5: Divide data into Training & Testing part
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=42,test_size=0.3)
+
+
+#step 6 : Train Model
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+
+# Show data sample 
+st.write(df.head())
+
+
+#create side bar for user input form
+st.sidebar.title("Fill Customer Details")
+st.sidebar.image(img_url)
+
+all_ans = []
+
+for index,col_name in enumerate(X.columns):
+    min_v = X[col_name].min()
+    max_v = X[col_name].max()
+
+    if col_name != "Previous_Insurance":
+        value = st.sidebar.slider(
+            f"select value for {col_name}",
+            min_value = int(min_v),
+            max_value = int(max_v)
+        )
+    else:
+        value = st.sidebar.number_input(
+            f"Select value for {col_name} (0: No, 1: Yes)",
+            min_value = 0,
+            max_value = 1,
+            step = 1
+        )
+
+    all_ans.append(value)
+
+ud = {j:all_ans[i] for i,j in enumerate(X.columns)}
+user_df = pd.DataFrame([all_ans], columns = X.columns)
+st.write(user_df)
+
+
+#====================================================================prediction==============================================================================================================================#
+
+if st.button("CLick to Predict"):
+    with st.spinner("predicting.."):
+        import time
+        time.sleep(2)
+
+    final_ans = model.predict([user_df])[0]
+
+    if final_ans == 0:
+        st.info("✖️customer will not buy the insurance✖️")
+    else:
+        st.success("☑️customer will buy the insurance☑️")
